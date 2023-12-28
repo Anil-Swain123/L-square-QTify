cd80f45728c207cd9ffd12ce4faa0dcad51e57ca
Anil-Swain123 <anilkumarswn@gmail.com>
2023-12-28 09:44:09 +0530

    Cards Added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import { StyledEngineProvider } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from './API/API';
+
 
 function App() {
+
+  const [searchData, setSearchData] = useState();
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) =>{
+    source().then((data)=>{
+    setData((prevData)=>{
+      return {...prevData, [key]: data};
+    })
+  })
+  }
+
+  useEffect(()=>{
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+  }, [])
+
+  const {topAlbums=[], newAlbums=[], songs =[]} = data;
+
   return (
-    <><Navbar searchData={""} /> < Hero /></>
+    <>
+    <StyledEngineProvider injectFirst>
+    <Navbar searchData={[...topAlbums, ...newAlbums]} />
+    <Outlet context={{data:{topAlbums, newAlbums, songs}}} />
+    </StyledEngineProvider>
+    </>
   );
 }
 
--- qtify/src/index.js
+++ qtify/src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HomePage from "./pages/HomePage/HomePage";
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router ={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
