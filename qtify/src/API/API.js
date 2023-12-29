c0113f45e62d24cb5ba0ee3d9bcc1b39b8943f1d
Anil-Swain123 <anilkumarswn@gmail.com>
2023-12-29 09:55:12 +0530

    songs added

--- qtify/src/API/API.js
+++ qtify/src/API/API.js
@@ -20,7 +20,7 @@ export const fetchNewAlbums = async () =>{
 }
 export const fetchSongs = async () =>{
     try {
-        const response = await axios.get(`${BACKEND_ENDPOINT}/albums/songs`);
+        const response = await axios.get(`${BACKEND_ENDPOINT}/songs`);
         return response.data;
     } catch(e){
         console.log(e);
@@ -28,7 +28,7 @@ export const fetchSongs = async () =>{
 }
 export const fetchFilters = async () =>{
     try {
-        const response = await axios.get(`${BACKEND_ENDPOINT}/albums/genre`);
+        const response = await axios.get(`${BACKEND_ENDPOINT}/genres`);
         return response.data;
     } catch(e){
         console.log(e);
